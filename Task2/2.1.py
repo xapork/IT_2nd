e3c3b52fa98f2a02c1ae3de8c925d5f0855e22e3
Justina Romanenko <150509333+xapork@users.noreply.github.com>
2024-05-06 21:58:57 +0500

    2.1.py

--- Task2/2.1.py
+++ Task2/2.1.py
@@ -1,1 +1,10 @@
-
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    
+    def get_title(self):
+        return self.title
+    
+    def get_author(self):
+        return self.author
