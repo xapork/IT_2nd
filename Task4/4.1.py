b0735d24f6526a8e781fe86d18eac62b0d4501bf
Justina Romanenko <150509333+xapork@users.noreply.github.com>
2024-05-19 23:37:50 +0500

    4.1.py

---
+++ Task4/4.1.py
@@ -0,0 +1,10 @@
+import itertools
+
+def combinations(word: str):
+    combinations = set()
+    for i in range(1, len(word) + 1):
+        for combination in itertools.permutations(word, i):
+            combinations.add(''.join(combination))
+    return combinations
+
+print(combinations("привет"))
